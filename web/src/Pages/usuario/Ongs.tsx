a20abe0fbb5b5ba1b0eba6ecadd0373a75513a97
GabrielAlbanez <gabriel.g.abanez@gmail.com>
2023-11-01 10:22:37 -0300

    add tela de ong para o usuario

--- web/src/Pages/usuario/Ongs.tsx
+++ web/src/Pages/usuario/Ongs.tsx
@@ -50,22 +50,36 @@ export default function Ongs() {
   console.log(url);
 
   return (
-    <div>
+    <div className="w-full h-[91vh] bg">
       {dataOng.length > 0 ? (
-        <>
-          <img src={require(`../../uploads/${url}`)} alt="" />
-
-          <h1>name ong : {dataOng[0]?.nome}</h1>
-        </>
+        <div className="h-full w-full">
+          <section className="w-full  h-[20%] flex gap-2 items-center justify-center pt-7">
+            <img
+              src={require(`../../uploads/${url}`)}
+              alt=""
+              className="w-20 h-20 rounded-full mr-4"
+            />
+            <p className="text-2xl ">{dataOng[0].nome}</p>
+          </section>
+          <section className="w-full flex justify-between pt-16 h-[70%] ">
+            <div className="w-[50%] h-full flex  justify-center border-r-[1px] border-black">
+              <h2 className="text-2xl">Informaçoes Ong</h2>
+            </div>
+            <div className="w-[50%] h-full flex  justify-center">
+              <h1 className="text-2xl">Rifas</h1>
+              <div>
+              </div>
+            </div>
+          </section>
+        </div>
       ) : (
         <div className="xl:h-[91vh] w-full flex flex-col items-center justify-center gap-6">
-        <h1 className="text-3xl">Carregando Dados</h1>
+          <h1 className="text-3xl">Carregando Dados</h1>
 
-
-        <div className="animate-spin h-5   flex flex-col justify-center items-center  ">
-          <ImSpinner2 />
+          <div className="animate-spin h-5   flex flex-col justify-center items-center  ">
+            <ImSpinner2 />
+          </div>
         </div>
-      </div>
       )}
     </div>
   );
