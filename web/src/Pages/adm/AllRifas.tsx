c082821c11405e29bee9c6992042b90301319148
GabrielAlbanez <gabriel.g.abanez@gmail.com>
2023-11-10 12:09:55 -0300

    add tabelas com sucesso e com filtro

--- web/src/Pages/adm/AllRifas.tsx
+++ web/src/Pages/adm/AllRifas.tsx
@@ -1,10 +1,7 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import BasicTable from '../../components/TableDashboard';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function AllRifas() {
-
-
   type dataRifa = {
     id: number;
     imgRifa: string;
@@ -24,30 +21,92 @@ export default function AllRifas() {
       aprovado: boolean;
       Logo: string;
     };
-    NumeroComprado: {
-      id: number;
-      numero: number;
-      rifaId: number;
-      usuarioCpf: string;
-    };
-  }
+    NumeroComprado: [
+      {
+        id: number;
+        numero: number;
+        rifaId: number;
+        usuarioCpf: string;
+      }
+    ];
+  };
 
+  const [dataRifa, setDataRifa] = useState<dataRifa[]>([]);
 
+  const [buscarData, setBuscarData] = useState("");
 
-  const [dataRifa,seDataRifa] = useState<dataRifa>()
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBuscarData(event.target.value);
+  };
 
+  const filteredRifas = dataRifa.filter((rifa) =>
+    rifa.nome.toLowerCase().includes(buscarData.toLowerCase())
+  );
 
   useEffect(() => {
     const url = "http://localhost:8080/getAllRifas";
     axios.get(url).then((response) => {
       const data = response.data;
-      seDataRifa(data.rifas[0]);
+      setDataRifa(data.rifas);
     });
   }, []);
 
+  console.log(dataRifa);
+
   return (
-    <div>
-       <BasicTable rifas={dataRifa}/>
+    <div className="mt-8">
+      <h2 className="text-2xl font-bold mb-4">Rifas List</h2>
+      <div className="mb-4">
+        <label htmlFor="search" className="mr-2">
+          Busca:
+        </label>
+        <input
+          type="text"
+          id="search"
+          onChange={handleSearch}
+          value={buscarData}
+          className="p-2 border border-gray-300"
+        />
+      </div>
+      <table className="min-w-full border border-gray-300">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="py-2 px-4 border-b">ID</th>
+            <th className="py-2 px-4 border-b">Nome</th>
+            <th className="py-2 px-4 border-b">Preço</th>
+            <th className="py-2 px-4 border-b">Descrição</th>
+            <th className="py-2 px-4 border-b">ID da Ong</th>
+            <th className="py-2 px-4 border-b">Numeros Comprados</th>
+            <th className="py-2 px-4 border-b">Cpf User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredRifas.map((rifa) => (
+            <tr key={rifa.id} className="border-b text-center">
+              <td className="py-2 px-4">{rifa.id}</td>
+              <td className="py-2 px-4">{rifa.nome}</td>
+              <td className="py-2 px-4">{rifa.preco}</td>
+              <td className="py-2 px-4">{rifa.descricao}</td>
+              <td className="py-2 px-4">{rifa.idOng}</td>
+              <td className="py-2 px-4 ">
+                {dataRifa[0]?.NumeroComprado.map((numero) => (
+                  <div className="">
+                    <p>{numero.numero}</p>
+                  </div>
+                ))}
+              </td>
+
+              <td className="py-2 px-4 ">
+                {dataRifa[0]?.NumeroComprado.map((numero) => (
+                  <div className="">
+                    <p>{numero.usuarioCpf}</p>
+                  </div>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
--- web/src/Pages/adm/AllUsers.tsx
+++ web/src/Pages/adm/AllUsers.tsx
@@ -1,30 +1,27 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../store/intex';
-import BasicTable from '../../components/TableDashboard';
 
 export default function AllUsers() {
-
-
-
   type dataUser = {
     cpf: string;
-      nome: string;
-      email: string;
-      senha: string;
-      sexo: string;
-      endereco: string;
-      tipo: string;
-      telefone: string;
-      numerosComprados: {
-        id: number;
-        numero: number;
-        rifaId: number;
-        usuarioCpf: string;
-      };
-  }
+    nome: string;
+    email: string;
+    senha: string;
+    sexo: string;
+    endereco: string;
+    tipo: string;
+    telefone: string;
+    numerosComprados: {
+      id: number;
+      numero: number;
+      rifaId: number;
+      usuarioCpf: string;
+    };
+  };
 
-  const [dataUsers,setDataUsers] = useState<dataUser>()
+  const [dataUsers, setDataUsers] = useState<dataUser[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const User = useAppSelector((state) => state.AuthToken.dataUser) as Array<{
     cpf: string;
@@ -34,17 +31,65 @@ export default function AllUsers() {
     tipo: string;
   }>;
 
-  const cpf = User[0]?.cpf
+  const cpf = User[0]?.cpf;
 
   useEffect(() => {
     const url = `http://localhost:8080/allUsers/${cpf}`;
     axios.get(url).then((response) => {
       const data = response.data;
-      setDataUsers(data.users[0]);
+      setDataUsers(data.users);
     });
   }, []);
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredUsers = dataUsers.filter((user) =>
+    user.nome.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <BasicTable usuarios={dataUsers}/>
-  )
+    <div className="mt-8">
+      <h2 className="text-2xl font-bold mb-4">Lista de Usuarios</h2>
+      <div className="mb-4">
+        <label htmlFor="search" className="mr-2">
+          Busca:
+        </label>
+        <input
+          type="text"
+          id="search"
+          onChange={handleSearch}
+          value={searchTerm}
+          className="p-2 border border-gray-300"
+        />
+      </div>
+      <table className="min-w-full border border-gray-300">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="py-2 px-4 border-b">CPF</th>
+            <th className="py-2 px-4 border-b">Nome</th>
+            <th className="py-2 px-4 border-b">Email</th>
+            <th className="py-2 px-4 border-b">Sexo</th>
+            <th className="py-2 px-4 border-b">Endereço</th>
+            <th className="py-2 px-4 border-b">Tipo</th>
+            <th className="py-2 px-4 border-b">Telefone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredUsers.map((user) => (
+            <tr key={user.cpf} className="border-b text-center">
+              <td className="py-2 px-4">{user.cpf}</td>
+              <td className="py-2 px-4">{user.nome}</td>
+              <td className="py-2 px-4">{user.email}</td>
+              <td className="py-2 px-4">{user.sexo}</td>
+              <td className="py-2 px-4">{user.endereco}</td>
+              <td className="py-2 px-4">{user.tipo}</td>
+              <td className="py-2 px-4">{user.telefone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
