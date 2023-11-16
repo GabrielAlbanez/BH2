af7258cbeb28571d8293a6a6cb2ad6acb17bba8b
GabrielAlbanez <gabriel.g.albanez@gmail.com>
2023-11-16 16:40:05 -0300

    tela de criar rifas responsiva

--- web/src/Pages/ongs/RifasOngs.tsx
+++ web/src/Pages/ongs/RifasOngs.tsx
@@ -18,17 +18,17 @@ export default function RifasOngs() {
 
   const [imgRifa, setImgRifa] = useState<File | null>(null);
 
-
   const ong = useAppSelector((state) => state.AuthToken.dataOng) as Array<{
     cnpj: string;
   }>;
 
+  localStorage.setItem("cnpjOng", ong[0]?.cnpj);
 
-  localStorage.setItem('cnpjOng',ong[0]?.cnpj)
+  const navigator = useNavigate();
 
-  const navigator = useNavigate()
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
     const { name, value } = e.target;
     setDataRegisterRifa((rifas) => ({
       ...rifas,
@@ -36,181 +36,170 @@ export default function RifasOngs() {
     }));
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     if (e.target.files && e.target.files.length > 0) {
       setImgRifa(e.target.files[0]);
     }
-    console.log(imgRifa);
   };
 
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
     e.preventDefault();
-  
-    // Converter "preco" em um número de ponto flutuante
+
     const preco = parseFloat(dataRegisterRifa.preco);
-  
-    // Verificar se a conversão para float foi bem-sucedida
+
     if (isNaN(preco)) {
-      console.log("Preço inválido. Por favor, insira um número válido.");
+      console.log(
+        "Preço inválido. Por favor, insira um número válido."
+      );
       return;
     }
-  
-    console.log(dataRegisterRifa);
-    console.log(imgRifa);
-  
+
     const formData = new FormData();
-  
+
     formData.append("nome", dataRegisterRifa.nome);
     formData.append("descricaon", dataRegisterRifa.descricaon);
-  
 
     formData.append("preco", dataRegisterRifa.preco);
-    formData.append("cnpj",ong[0]?.cnpj)
-  
+    formData.append("cnpj", ong[0]?.cnpj);
+
     if (imgRifa) {
       formData.append("imgRifa", imgRifa);
     }
-  
+
     try {
-      const request = await axios.post(`http://localhost:8080/createRifas`, formData);
-  
+      const request = await axios.post(
+        `http://localhost:8080/createRifas`,
+        formData
+      );
+
       const responseData = request.data;
-  
-      console.log(responseData);
-      
 
+      if ("error" in responseData) {
+        const notify = () => {
+          toast(`${responseData.error}`, {
+            icon: `${
+              pegarTema === "dark" ? "🌑" : " 🌞"
+            }`,
+            style: {
+              borderRadius: "10px",
+              background: `${
+                pegarTema === "dark" ? "#333" : "white"
+              }`,
+              color: `${
+                pegarTema === "dark" ? "white" : "black"
+              }`,
+            },
+          });
+        };
 
-     if("error" in responseData) {
-      const notify = () => {
-        toast(`${responseData.error}`, {
-          icon: `${pegarTema === "dark" ? "🌑" : " 🌞"}`,
-          style: {
-            borderRadius: "10px",
-            background: `${pegarTema === "dark" ? "#333" : "white"}`,
-            color: `${pegarTema === "dark" ? "white" : "black"}`,
-          },
-        });
-      };
+        notify();
+      } else {
+        const notify = () => {
+          toast(`Rifa criada com sucesso`, {
+            icon: `${
+              pegarTema === "dark" ? "🌑" : " 🌞"
+            }`,
+            style: {
+              borderRadius: "10px",
+              background: `${
+                pegarTema === "dark" ? "#333" : "white"
+              }`,
+              color: `${
+                pegarTema === "dark" ? "white" : "black"
+              }`,
+            },
+          });
+        };
+        setTimeout(() => {
+          navigator("/HomeOng");
+        }, 3000);
 
-      notify();
-    }
-
-    else {
-      const notify = () => {
-        toast(`Rifa criada com sucesso`, {
-          icon: `${pegarTema === "dark" ? "🌑" : " 🌞"}`,
-          style: {
-            borderRadius: "10px",
-            background: `${pegarTema === "dark" ? "#333" : "white"}`,
-            color: `${pegarTema === "dark" ? "white" : "black"}`,
-          },
-        });
-      };
-      setTimeout(()=>{
-       
-        navigator('/HomeOng')
-
-      },3000)
-      // window.location.reload()
-      notify();
-      
-      // window.location.reload()
-      ;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        notify();
+      }
     } catch (error) {
-      console.log('Falha ao registrar uma rifa', error);
+      console.log("Falha ao registrar uma rifa", error);
     }
-  }
-
-    
-
-
-  
+  };
 
   return (
     <div
-      className={` w-full h-[91vh] transition-all duration-1000 flex justify-center items-center flex-col gap-16  ${
-        pegarTema === "dark" ? "bg-[#202020] text-white" : "bg-[#CEF3FF]"
+      className={`w-full min-h-screen transition-all duration-1000 flex justify-center items-center flex-col gap-16 ${
+        pegarTema === "dark"
+          ? "bg-black text-white"
+          : "bg-[#CEF3FF]"
       }`}
     >
       <h1 className="text-center text-4xl">Criar Rifas</h1>
-      <form onSubmit={handleSubmit} className="flex  w-[50%] h-[80%] shadow-xl border-fuchsia-500 border-[1px] rounded-3xl flex-col items-center justify-center px-22 py-20 gap-12 ">
-        <div className="flex gap-2 flex-col ">
-          <label htmlFor="">Imagen do Premio da Rifa</label>
-          <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full md:w-[70%] lg:w-[50%] xl:w-[40%] h-[80%] shadow-xl border-fuchsia-500 border-[1px] rounded-3xl flex-col items-center justify-center px-10 py-8 gap-8"
+      >
+        <div className="flex flex-col w-full">
+          <label htmlFor="imgRifa">Imagem do Prêmio da Rifa</label>
+          <div className="w-full border-purple-500 border-[1px] flex items-center justify-center h-[8vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500">
             <input
               onChange={handleFileChange}
               type="file"
               id="imgRifa"
               accept="image/*"
-              className="w-[93%] rounded-full h-[100%] border-white outline-0 bg-transparent p-3"
+              className="w-[93%] rounded-full h-[70%] border-white outline-0 bg-transparent p-3"
             />
           </div>
         </div>
 
-        <div className="flex gap-2 flex-col ">
-          <label htmlFor="">Nome Da Rifa</label>
-          <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+        <div className="flex flex-col w-full">
+          <label htmlFor="nome">Nome Da Rifa</label>
+          <div className="w-full border-purple-500 border-[1px] flex items-center justify-center h-[8vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500">
             <input
               name="nome"
               type="text"
               value={dataRegisterRifa.nome}
               onChange={handleInputChange}
-              placeholder="name@example.com.."
-              className="w-[93%] rounded-full h-[60%] border-white outline-0 bg-transparent p-3"
+              placeholder="Nome da Rifa"
+              className="w-[93%] rounded-full h-[90%] border-white outline-0 bg-transparent p-3"
             />
           </div>
         </div>
 
-        <div className="flex gap-2 flex-col ">
-          <label htmlFor="">Descirção da Rifa</label>
-          <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+        <div className="flex flex-col w-full">
+          <label htmlFor="descricaon">Descrição da Rifa</label>
+          <div className="w-full border-purple-500 border-[1px] flex items-center justify-center h-[8vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500">
             <input
               name="descricaon"
               type="text"
               value={dataRegisterRifa.descricaon}
               onChange={handleInputChange}
-              placeholder="name@example.com.."
-              className="w-[93%] rounded-full h-[60%] border-white outline-0 bg-transparent p-3"
+              placeholder="Descrição da Rifa"
+              className="w-[93%] rounded-full h-[90%] border-white outline-0 bg-transparent p-3"
             />
           </div>
         </div>
 
-        <div className="flex gap-2 flex-col ">
-          <label htmlFor="">Preço do numero de Cada Rifa</label>
-          <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+        <div className="flex flex-col w-full">
+          <label htmlFor="preco">Preço do Número de Cada Rifa</label>
+          <div className="w-full border-purple-500 border-[1px] flex items-center justify-center h-[8vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500">
             <input
               name="preco"
               type="number"
               value={dataRegisterRifa.preco}
               onChange={handleInputChange}
-              placeholder="name@example.com.."
-              className="w-[93%] rounded-full h-[60%] border-white outline-0 bg-transparent p-3"
+              placeholder="Preço da Rifa"
+              className="w-[93%] rounded-full h-[90%] border-white outline-0 bg-transparent p-3"
             />
           </div>
         </div>
 
         <div>
-          <button type="submit">Criar Rifa </button>
+          <button
+            type="submit"
+            className="bg-fuchsia-400 hover:bg-fuchsia-700 text-white font-bold py-2 px-4 rounded-full transition-all"
+          >
+            Criar Rifa
+          </button>
         </div>
       </form>
     </div>
