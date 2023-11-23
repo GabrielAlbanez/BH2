bef0e2915c595aea5aad371865b9d226c5267a15
GabrielAlbanez <gabriel.g.abanez@gmail.com>
2023-11-23 11:43:10 -0300

    add setup tela account usuario

--- web/src/Pages/usuario/Account.tsx
+++ web/src/Pages/usuario/Account.tsx
@@ -6,6 +6,8 @@ import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import sockett from "../../common/io/io";
+import defaulImg from "../../assets/imgs/img avatar.png";
+import Button from "../../components/Button";
 
 export default function Account() {
   const { pegarTema } = useTema() as {
@@ -18,7 +20,7 @@ export default function Account() {
     numerosComprados: [];
     sexo: string;
     tipo: string;
-    nome : string;
+    nome: string;
   }>;
   const logedUser = useAppSelector((state) => state.AuthToken.isLoged);
 
@@ -53,37 +55,97 @@ export default function Account() {
     usuarioCpf: string;
   };
 
- 
   return (
     <div
       className={` w-full h-[91vh] transition-all duration-1000 flex items-center justify-center flex-col gap-10 ${
-        pegarTema === "dark" ? "bg-black text-white" : "bg-[#CEF3FF]"
+        pegarTema === "dark" ? "bg-black text-white" : "bg-[#CEF3FF] text-black"
       }`}
     >
-      <div className="text-4xl">Account</div>
+      <div className="text-4xl pt-10">Account</div>
       {logedUser === "true" ? (
         <>
           {logedUser === "true" || User.length > 0 ? (
-            <div className="flex flex-col gap-10 items-center justify-center h-[70%] w-[100%] rounded-2xl  text-lg ">
-              <p>nome: {User[0]?.nome}</p>
-              <p>Cpf : {User[0]?.cpf}</p>
-              <p>Email : {User[0]?.email}</p>
-              
-              <p>Sexo : {User[0]?.sexo}</p>
-              {User[0]?.tipo === "admin" ? (
-                <>
-                  <p>Type : Admin</p>
-                </>
-              ) : (
-                <></>
-              )}
+            <div className="flex   items-center justify-center gap-32 h-[100%] w-[100%] rounded-2xl  text-lg ">
+              <div className="w-full h-full   text-white flex items-center justify-center">
+                <figure className="border-[3px] border-black rounded-full bg-white px-5 py-5">
+                  <img
+                    src={defaulImg}
+                    alt=""
+                    className="rounded-full w-80 h-80 object-cover transition-all duration-1000 hover:scale-150  hover:overflow-hidden"
+                  />
+                </figure>
+              </div>
 
-              <button
-                onClick={logOut}
-                className="border-violet-400 border-[2px] rounded-2xl px-4 py-4"
+              <form
+                className={`w-full h-full  text-white flex justify-center gap-10 items-center flex-col ${
+                  pegarTema === "dark" ? "text-white" : "text-black"
+                }`}
               >
-                Logout
-              </button>
+                <div
+                  className={`flex gap-2 flex-col  ${
+                    pegarTema === "dark" ? "text-white" : "text-black"
+                  }`}
+                >
+                  <label htmlFor="">nome</label>
+                  <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+                    <input
+                      name="nome"
+                      type="text"
+                      defaultValue={User[0]?.nome}
+                      className="w-[93%] rounded-full h-[60%] border-white outline-0 bg-transparent p-3"
+                    />
+                  </div>
+                </div>
+                <div   className={`flex gap-2 flex-col  ${
+                    pegarTema === "dark" ? "text-white" : "text-black"
+                  }`}
+                >
+                  <label htmlFor="">cpf</label>
+                  <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+                    <input
+                      name="cpf"
+                      defaultValue={User[0]?.cpf}
+                      type="text"
+                      className="w-[93%] rounded-full h-[60%] border-white outline-0 bg-transparent p-3"
+                    />
+                  </div>
+                </div>
+                <div   className={`flex gap-2 flex-col  ${
+                    pegarTema === "dark" ? "text-white" : "text-black"
+                  }`}
+                >
+                  <label htmlFor="">email</label>
+                  <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+                    <input
+                      name="email"
+                      type="text"
+                      defaultValue={User[0]?.email}
+                      placeholder="name@example.com.."
+                      className="w-[93%] rounded-full h-[60%] border-white outline-0 bg-transparent p-3"
+                    />
+                  </div>
+                </div>
+
+                <div   className={`flex gap-2 flex-col  ${
+                    pegarTema === "dark" ? "text-white" : "text-black"
+                  }`}
+                >
+                  <label htmlFor="">sexo</label>
+                  <div className="w-[100%] sm:w-[70vh] md:w-[60vh] border-purple-500 border-[1px] flex items-center justify-center h-[6vh]  2xl:h-[6vh] rounded-2xl transition shadow-purple-300 shadow-md hover:shadow-lg hover:shadow-purple-500 ">
+                    <input
+                      name="sexo"
+                      type="text"
+                      defaultValue={User[0]?.sexo}
+                      placeholder="name@example.com.."
+                      className="w-[93%] rounded-full h-[60%] border-white outline-0 bg-transparent p-3"
+                    />
+                  </div>
+                </div>
+
+                <button className={`p-6 border-[2px] border-fuchsia-500 rounded-full px-5 py-2 ${pegarTema === 'dark' ? "text-white" : "text-black"}`}>
+                  update profile
+                </button>
+              </form>
             </div>
           ) : (
             <h1>Carregando dados do usuário...</h1>
