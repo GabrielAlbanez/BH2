d9efb3afd51a2544be3f527fffa653b4785acd05
GabrielAlbanez <gabriel.g.albabez@gmail.com>
2023-12-05 09:10:07 -0300

    remove credentilas cors sockeIo

--- Server/src/SocketIo/socket.ts
+++ Server/src/SocketIo/socket.ts
@@ -7,7 +7,7 @@ const configureSocketIO = (httpServer) => {
     cors: {
       origin: "https://be-human-five.vercel.app",
       methods: ["GET", "POST"],
-      credentials: true
+      credentials: false
     }
   });
 
