7fdf09b6ab8fe0c591c3d80f2eb523f543677d45
GabrielAlbanez <gabriel.g.albabez@gmail.com>
2023-12-06 18:40:17 -0300

    add loading pagina rifas e sorteios

--- web/src/Pages/usuario/Rifas.tsx
+++ web/src/Pages/usuario/Rifas.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useAppSelector } from "../../store/intex";
 import toast from "react-hot-toast";
 import { useTema } from "../../common/context/Tema";
+import { ImSpinner2 } from "react-icons/im";
 
 export default function Rifas() {
   const User = useAppSelector((state) => state.AuthToken.dataUser) as Array<{
@@ -25,14 +26,14 @@ export default function Rifas() {
           nome: string;
           preco: number;
           descricao: string;
-          ganhador : string
-          sorteado : true;
+          ganhador: string
+          sorteado: true;
           ong: {
             nome: string
           }
         };
 
-   
+
       }
     ];
   };
@@ -52,7 +53,7 @@ export default function Rifas() {
     };
 
     fetchData();
-  }, [User[0]?.cpf,dataNumerosComprados]);
+  }, [User[0]?.cpf, dataNumerosComprados]);
 
 
 
@@ -72,83 +73,91 @@ export default function Rifas() {
     });
   };
 
-  const verySortRifa = (sorteado : boolean)=>{
-    if(sorteado === true){
+  const verySortRifa = (sorteado: boolean) => {
+    if (sorteado === true) {
       notify('essa rifa ja foi sorteada')
     } else {
       notify('essa rifa ainda n foi sorteada')
     }
-}
+  }
 
 
   return (
-    <div className={`transition-all py-10 duration-1000 w-full min-h-[100vh] sm:min-h-[91vh]  ${
-      pegarTema === "dark" ? "bg-black text-white" : "bg-[#CEF3FF]"
-    }`}>
+    <div className={`transition-all py-10 duration-1000 w-full min-h-[100vh] sm:min-h-[91vh]  ${pegarTema === "dark" ? "bg-black text-white" : "bg-[#CEF3FF]"
+      }`}>
       <h1 className="text-3xl text-center " >Minhas rifas</h1>
 
-      {dataNumerosComprados && dataNumerosComprados[0]?.numerosComprados.length > 0 ? (
-        <div className=" pt-10 h-full  w-full flex gap-5 flex-wrap items-center justify-center">
-          {dataNumerosComprados.map((numero, outerIndex) => (
-            <React.Fragment key={outerIndex}>
-              {numero.numerosComprados.map((valor, innerIndex) => (
-                
-                <div key={innerIndex} onClick={()=>{verySortRifa(valor.rifa.sorteado)}} className={`m-4 w-[300px] cursor-pointer  `}>
-                  {valor.rifa.ganhador === User[0]?.cpf ? (<>
-                    
-                    <div className={`max-w-sm rounded-2xl overflow-hidden shadow-xl mx-auto bg-white text-black  hover:scale-110 opacity-60 transition-all hover:opacity-100`}>
-                    <img
-                      src={`https://bh2-upl7.onrender.com/uploadsImgRifas/${valor.rifa.imgRifa.slice(16)}`}
-                      alt=""
-                      className="w-full h-48 object-cover transform transition-transform duration-[1500ms] hover:scale-110"
-                    />
-                      
-                    <div className="px-6 py-4 w-full flex justify-center items-center">
-                      {/* <h1 className="font-bold text-xl mb-2">Número: {valor.numero}</h1> */}
-                      <QRCode  value={JSON.stringify({
-                        "nome " : valor.rifa.nome,
-                        "numero" : valor.numero,
-                        "preço" : valor.rifa.preco,
-                        "ong" : valor.rifa.ong.nome
-                        
-                      })} />
-                    </div>
-                    <h1 className="text-md text-center">Essa Rifa ja foi Sorteada</h1>
+      {dataNumerosComprados && dataNumerosComprados[0]?.numerosComprados.length > 0 ? (<>
+        {dataNumerosComprados && dataNumerosComprados[0]?.numerosComprados.length > 0 ? (
+          <div className=" pt-10 h-full  w-full flex gap-5 flex-wrap items-center justify-center">
+            {dataNumerosComprados.map((numero, outerIndex) => (
+              <React.Fragment key={outerIndex}>
+                {numero.numerosComprados.map((valor, innerIndex) => (
+
+                  <div key={innerIndex} onClick={() => { verySortRifa(valor.rifa.sorteado) }} className={`m-4 w-[300px] cursor-pointer  `}>
+                    {valor.rifa.ganhador === User[0]?.cpf ? (<>
+
+                      <div className={`max-w-sm rounded-2xl overflow-hidden shadow-xl mx-auto bg-white text-black  hover:scale-110 opacity-60 transition-all hover:opacity-100`}>
+                        <img
+                          src={`https://bh2-upl7.onrender.com/uploadsImgRifas/${valor.rifa.imgRifa.slice(16)}`}
+                          alt=""
+                          className="w-full h-48 object-cover transform transition-transform duration-[1500ms] hover:scale-110"
+                        />
+
+                        <div className="px-6 py-4 w-full flex justify-center items-center">
+                          {/* <h1 className="font-bold text-xl mb-2">Número: {valor.numero}</h1> */}
+                          <QRCode value={JSON.stringify({
+                            "nome ": valor.rifa.nome,
+                            "numero": valor.numero,
+                            "preço": valor.rifa.preco,
+                            "ong": valor.rifa.ong.nome
+
+                          })} />
+                        </div>
+                        <h1 className="text-md text-center">Essa Rifa ja foi Sorteada</h1>
+
+                      </div>
+
+                    </>) : (<>
+                      <div className={`max-w-sm rounded-2xl overflow-hidden shadow-xl mx-auto  hover:scale-110 transition-all bg-white text-black ${valor.rifa.sorteado === true && valor.rifa.ganhador !== User[0]?.cpf ? 'opacity-60' : 'opacity-100'} `}>
+                        <img
+                          src={`https://bh2-upl7.onrender.com/uploadsImgRifas/${valor.rifa.imgRifa.slice(16)}`}
+                          alt=""
+                          className="w-full h-48 object-cover transform transition-transform duration-[1500ms] hover:scale-110"
+                        />
+                        <div className="px-6 py-4 flex items-center justify-center">
+                          {/* <h1 className="font-bold text-xl mb-2">Número: {valor.numero}</h1> */}
+                          <QRCode value={JSON.stringify({
+                            "nome ": valor.rifa.nome,
+                            "numero": valor.numero,
+                            "preço": valor.rifa.preco,
+                            "ong": valor.rifa.ong.nome
+
+                          })} />
+
+                        </div>
+                        {valor.rifa.sorteado === true && valor.rifa.ganhador !== User[0]?.cpf ? (<h1 className="text-center">Essa Rifa ja foi Sorteada</h1>) : (<></>)}
+
+                      </div></>)}
 
                   </div>
-
-                  </>) : (<>
-                    <div className={`max-w-sm rounded-2xl overflow-hidden shadow-xl mx-auto  hover:scale-110 transition-all bg-white text-black ${valor.rifa.sorteado === true && valor.rifa.ganhador !== User[0]?.cpf ? 'opacity-60' : 'opacity-100'} `}>
-                    <img
-                      src={`https://bh2-upl7.onrender.com/uploadsImgRifas/${valor.rifa.imgRifa.slice(16)}`}
-                      alt=""
-                      className="w-full h-48 object-cover transform transition-transform duration-[1500ms] hover:scale-110"
-                    />
-                    <div className="px-6 py-4 flex items-center justify-center">
-                      {/* <h1 className="font-bold text-xl mb-2">Número: {valor.numero}</h1> */}
-                      <QRCode  value={JSON.stringify({
-                        "nome " : valor.rifa.nome,
-                        "numero" : valor.numero,
-                        "preço" : valor.rifa.preco,
-                        "ong" : valor.rifa.ong.nome
-                        
-                      })} />
-
-                    </div>
-                    {valor.rifa.sorteado === true && valor.rifa.ganhador !== User[0]?.cpf ? (<h1 className="text-center">Essa Rifa ja foi Sorteada</h1>) : (<></>)}
-
-                  </div></>)}
-           
-                </div>
-              ))}
-            </React.Fragment>
-          ))}
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        ) : (
+          <div className="flex items-center justify-center h-[91vh] w-full text-3xl">
+            <h1>Você ainda não comprou nenhuma rifa</h1>
+          </div>
+        )}
+      </>) : (<div className="w-full h-[91vh] flex flex-col items-center justify-center">
+        <h1 className="text-3xl">Carregando Dados</h1>
+        <div className="animate-spin h-5 mt-4">
+          <ImSpinner2 />
         </div>
-      ) : (
-        <div className="flex items-center justify-center h-[91vh] w-full text-3xl">
-          <h1>Você ainda não comprou nenhuma rifa</h1>
-        </div>
-      )}
+      </div>)}
+
+
     </div>
   );
 }
--- web/src/Pages/usuario/SorteiosRifas.tsx
+++ web/src/Pages/usuario/SorteiosRifas.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useTema } from '../../common/context/Tema';
 import toast from 'react-hot-toast';
 import axios from 'axios';
+import { ImSpinner2 } from 'react-icons/im';
 
 export default function SorteiosRifas() {
   type dataRifaSorteada = {
@@ -61,50 +62,58 @@ export default function SorteiosRifas() {
       className={`w-full min-h-[100vh] sm:min-h-[91vh] transition-all duration-1000 flex flex-col items-center justify-center gap-5 py-5 ${pegarTema === 'dark' ? 'bg-black text-white' : 'bg-[#CEF3FF]'
         }`}
     >
-      <h1 className="text-4xl font-bold mb-12">Sorteios</h1>
-      {dataRifaSorteada.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4">
-          {dataRifaSorteada.map((rifa, index) => (
-            <div
-              key={rifa.ganhador}
-              className="max-w-sm  overflow-hidden shadow-lg bg-white hover:shadow-xl transition-all duration-[1700ms] ease-in-out transform hover:-translate-y-10 rounded-2xl"
-            >
-              {url.length > 0 && (<img className="w-full h-60 object-cover transform transition-transform duration-[1700ms] hover:scale-110"
-                src={`https://bh2-upl7.onrender.com/uploadsImgRifas/${url[index]}`}
+      {dataRifaSorteada.length > 0 ? (<>
+        <h1 className="text-4xl font-bold mb-12">Sorteios</h1>
+        {dataRifaSorteada.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4">
+            {dataRifaSorteada.map((rifa, index) => (
+              <div
+                key={rifa.ganhador}
+                className="max-w-sm  overflow-hidden shadow-lg bg-white hover:shadow-xl transition-all duration-[1700ms] ease-in-out transform hover:-translate-y-10 rounded-2xl"
+              >
+                {url.length > 0 && (<img className="w-full h-60 object-cover transform transition-transform duration-[1700ms] hover:scale-110"
+                  src={`https://bh2-upl7.onrender.com/uploadsImgRifas/${url[index]}`}
 
-                alt={rifa.nome} />)}
+                  alt={rifa.nome} />)}
 
-              <div className="p-4">
-                <h2 className="font-bold text-2xl text-gray-600  mb-2">{rifa.nome}</h2>
-                <p className="text-gray-600 text-sm">{rifa.descricao}</p>
-                <p className="text-gray-800 font-bold mt-2">preço: ${rifa.preco}</p>
-                {rifa.sorteado ? (
-                  <>
-                    <p className="text-green-500 font-bold mt-2">Sorteado</p>
-                    <p className="text-gray-800 font-bold mt-2">Ganhador: {rifa.ganhador}</p>
-                    {rifa.numeroSorteado && (
-                      <p className="text-gray-800 font-bold mt-2">
-                        Número Sorteado: {rifa.numeroSorteado}
-                      </p>
-                    )}
-                    {rifa.ganhador === cpf ? (
-                      <p className="text-green-500 font-bold mt-2">Parabéns, você ganhou esta rifa!</p>
-                    ) : (
-                      <p className="text-red-500 font-bold mt-2"></p>
-                    )}
-                  </>
-                ) : (
-                  <p className="text-orange-500 font-bold mt-2">Aguardando sorteio</p>
-                )}
+                <div className="p-4">
+                  <h2 className="font-bold text-2xl text-gray-600  mb-2">{rifa.nome}</h2>
+                  <p className="text-gray-600 text-sm">{rifa.descricao}</p>
+                  <p className="text-gray-800 font-bold mt-2">preço: ${rifa.preco}</p>
+                  {rifa.sorteado ? (
+                    <>
+                      <p className="text-green-500 font-bold mt-2">Sorteado</p>
+                      <p className="text-gray-800 font-bold mt-2">Ganhador: {rifa.ganhador}</p>
+                      {rifa.numeroSorteado && (
+                        <p className="text-gray-800 font-bold mt-2">
+                          Número Sorteado: {rifa.numeroSorteado}
+                        </p>
+                      )}
+                      {rifa.ganhador === cpf ? (
+                        <p className="text-green-500 font-bold mt-2">Parabéns, você ganhou esta rifa!</p>
+                      ) : (
+                        <p className="text-red-500 font-bold mt-2"></p>
+                      )}
+                    </>
+                  ) : (
+                    <p className="text-orange-500 font-bold mt-2">Aguardando sorteio</p>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        ) : (
+          <div className="w-full h-full flex items-center justify-center">
+            <h1 className="  text-center text-lg sm:text-xl  md:text-2xl">Ainda não foi realizado nenhum sorteio de nenhuma rifa, aguarde por favor...</h1>
+          </div>
+        )}
+      </>) : (<div className="w-full h-[91vh] flex flex-col items-center justify-center">
+        <h1 className="text-3xl">Carregando Dados</h1>
+        <div className="animate-spin h-5 mt-4">
+          <ImSpinner2 />
         </div>
-      ) : (
-        <div className="w-full h-full flex items-center justify-center">
-          <h1 className="  text-center text-lg sm:text-xl  md:text-2xl">Ainda não foi realizado nenhum sorteio de nenhuma rifa, aguarde por favor...</h1>
-        </div>
-      )}
+      </div>)}
+
     </div>
   );
 }
