338aaee97703d27757bd2aeeb15957130d8ec96a
GabrielAlbanez <gabriel.g.albabez@gmail.com>
2023-12-06 22:18:09 -0300

    add not found completa

--- web/src/Pages/404/index.tsx
+++ web/src/Pages/404/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useTema } from '../../common/context/Tema';
 
 export default function NotFoundPage() {
+
+
+
+  const { pegarTema } = useTema() as {
+    pegarTema: string;
+  };
+
+
+
   return (
-    <div className="min-h-screen flex flex-grow items-center justify-center bg-gray-50">
-      <div className="rounded-lg bg-white p-8 text-center shadow-xl">
+    <div className={`transition-all duration-1000 min-h-screen flex flex-grow items-center justify-center ${pegarTema === "dark" ? "bg-black text-white" : "bg-[#CEF3FF] text-black"}`}>
+      <div className="rounded-lg bg-white p-8 text-center shadow-2xl">
         <h1 className="mb-4 text-4xl font-bold">404</h1>
         <p className="text-gray-600">Oops! The page you are looking for could not be found.</p>
         <Link to={"/"}>
