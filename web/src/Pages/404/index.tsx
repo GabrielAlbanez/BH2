297771c921b013fb2ed6b9cc6fc911e299da9226
GabrielAlbanez <gabriel.g.albabez@gmail.com>
2023-12-06 22:21:08 -0300

    add colors correct not found

--- web/src/Pages/404/index.tsx
+++ web/src/Pages/404/index.tsx
@@ -13,7 +13,7 @@ export default function NotFoundPage() {
 
 
   return (
-    <div className={`transition-all duration-1000 min-h-screen flex flex-grow items-center justify-center ${pegarTema === "dark" ? "bg-black text-white" : "bg-[#CEF3FF] text-black"}`}>
+    <div className={`transition-all duration-1000 min-h-screen flex flex-grow items-center justify-center ${pegarTema === "dark" ? "bg-black " : "bg-[#CEF3FF] "}`}>
       <div className="rounded-lg bg-white p-8 text-center shadow-2xl">
         <h1 className="mb-4 text-4xl font-bold">404</h1>
         <p className="text-gray-600">Oops! The page you are looking for could not be found.</p>
